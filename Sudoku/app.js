3f42135f44ae9fb4c43b9a95762828d3ce7e9c34
lc <lc862304159@163.com>
2018-04-11 21:03:07 +0800

    提交

--- Sudoku/app.js
+++ Sudoku/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var ejs = require('ejs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -11,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine(".html",ejs.__express);
 
 app.use(logger('dev'));
 app.use(express.json());
